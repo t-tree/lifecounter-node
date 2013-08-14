33e0c1b2041dde02f8ac4844d6391d19a1a5183d
OpenShift guest <ttree.leaf@gmail.com>
2013-08-14 03:54:09 -0400

    delete unnecessary files

---
+++ public/javascripts/room.js
@@ -0,0 +1,86 @@
+$(function() {
+    var socket = io.connect(location.origin);
+
+    // サーバーと接続したタイミングでライフを取得（初期値はサーバーで生成している）
+    socket.on('connect', function() {
+        console.log('connected');
+        socket.emit('init');
+        socket.emit('getMyLife');
+        socket.emit('getLifeTable');
+        socket.emit('getMyRoomNumber');
+        console.log('socket.emit(getMyLife)');
+    });
+
+    // サーバーからライフを受け取って表示を更新する
+    socket.on('returnMyLife', function(life) {
+        console.log(life);
+        $("#life").text(life);
+    });
+
+    // サーバーから部屋番号を受け取って表示を更新する
+    socket.on('returnMyRoomNumber', function(room) {
+        console.log(room);
+        $("#roomNumber").text(room);
+    });
+
+
+    // 参加者全員のライフが送られてくるので更新する
+    socket.on('updateLifeTable', function(data) {
+        var tr = $("#life-table tbody").children(); //全行を取得
+
+        for (var i = 0, l = tr.length; i < l; i++) {
+            var cells = tr.eq(i).children(); //1行目から順にth、td問わず列を取得
+            var name = cells.eq(1).text();
+            if (typeof data[name] !== "undefined") {
+                cells.eq(2).text(data[name]);
+                delete data[name];
+            }
+            else {
+                tr.eq(i).remove();
+                continue;
+            }
+        }
+
+        for (var _name in data) {
+            $("#life-table").append(
+            $("<tr></tr>").append($("<td></td>").text("x")).append($("<td></td>").text(_name)).append($("<td></td>").text(data[_name])));
+        }
+    });
+
+
+    $("#reset").click(
+
+    function() {
+
+        socket.emit('lifeSet', 20);
+    });
+
+    $("#inc5").click(
+
+    function() {
+        $("#life").text(parseInt($("#life").text()) + 5);
+        socket.emit('lifeUpdate', 5);
+    });
+
+    $("#inc1").click(
+
+    function() {
+        $("#life").text(parseInt($("#life").text()) + 1);
+        socket.emit('lifeUpdate', 1);
+    });
+
+    $("#dec5").click(
+
+    function() {
+        $("#life").text(parseInt($("#life").text()) - 5);
+        socket.emit('lifeUpdate', - 5);
+    });
+
+    $("#dec1").click(
+
+    function() {
+        $("#life").text(parseInt($("#life").text()) - 1);
+        socket.emit('lifeUpdate', - 1);
+    });
+
+});
